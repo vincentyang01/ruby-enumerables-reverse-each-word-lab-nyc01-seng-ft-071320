98506345a1a15019b8cd79a55eb917b4ff9d8a5a
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-10 04:47:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,13 @@
 sentence = "Hello there, and how are you?"
 
 def reverse_each_word(sentence)
-  i = 0;
-
-  words = []
-  reverseWord = []
-  words = sentence.split(' ')
-  while i < words.length do
-    reverseWord << words[i].reverse
-    i += 1
-  end
-  reverseWord.join(" ")
+  #i = 0;
+  #words = []
+  #reverseWord = []
+  #words = sentence.split(' ')
+  #while i < words.length do
+  #  reverseWord << words[i].reverse
+  #  i += 1
+  #end
+  #reverseWord.join(" ")
 end
