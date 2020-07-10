be62b874e53d45c6fa297eff0304e455b04b17ee
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-10 04:53:59 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,22 @@
+require 'pry'
 sentence = "Hello there, and how are you?"
 
 def reverse_each_word(sentence)
-  i = 0;
-  words = []
-  reverseWord = []
-  words = sentence.split(' ')
-  while i < words.length do
-    reverseWord << words[i].reverse
-    i += 1
+  #i = 0;
+  #words = []
+  #reverseWord = []
+  #words = sentence.split(' ')
+  #while i < words.length do
+  #  reverseWord << words[i].reverse
+  #  i += 1
+  #end
+  #reverseWord.join(" ")
+  
+  words = sentence.split(" ")
+  testArray = []
+  words.collect do |string|
+    testArray << string.reverse
   end
-  reverseWord.join(" ")
+  testArray.join(" ")
+  
 end
